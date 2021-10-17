42b67500b42629a044afe4394e8e08531896b119
flyzgq <zgqtsyx@163.com>
2021-10-17 12:44:33 +0800

    修改swagger2配置

--- src/main/java/com/fly/zookeeper/config/SwaggerConf.java
+++ src/main/java/com/fly/zookeeper/config/SwaggerConf.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,23 +21,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConf {
-
+    //api接口包扫描路径
+    public static final String SWAGGER_SCAN_BASE_PACKAGE = "com.fly.zookeeper";
+    public static final String VERSION = "1.0.0";
     @Bean
-    public Docket createRestApi(){
+    public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
-                //.enable(true)
                 .select()
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
+                .paths(PathSelectors.any()) // 可以根据url路径设置哪些请求加入文档，忽略哪些请求
                 .build();
     }
-
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("zookeeper接口测试")
-                .description("接口说明")
-                .termsOfServiceUrl("http://localhost:8080")
-                .version("1.0")
+                .title("zookeeper客户端") //设置文档的标题
+                .description("zookeeper客户端 API 接口文档") // 设置文档的描述
+                .version(VERSION) // 设置文档的版本信息-> 1.0.0 Version information
+                .termsOfServiceUrl("http://www.baidu.com") // 设置文档的License信息->1.3 License information
                 .build();
     }
 }
